62c96c85461884446da639cc5a821425719ab6a2
Juan Salas <juandre1298@gmail.com>
2023-06-06 14:52:31 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 5000;
+
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+export const DB_USER = process.env.DB_HOST || "juan";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "1298";
+export const DB_NAME = process.env.DB_NAME || "new_schema";
+export const DB_PORT = process.env.DB_PORT || 3306;
