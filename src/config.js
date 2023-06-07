89de502545a4f19579126f2e7dbc12822ad6441f
Juan Salas <juandre1298@gmail.com>
2023-06-07 17:11:12 -0500

    try to push

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
-export const PORT = process.env.PORT || 5000;
+import * as dotenv from "dotenv";
+dotenv.config();
 
-export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
-export const DB_USER = process.env.DB_USER || "juan";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "1298";
-export const DB_NAME = process.env.DB_NAME || "new_schema";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const PORT = process.env.PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_PORT = process.env.DB_PORT;
